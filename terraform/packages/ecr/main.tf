06205cd7a002df0a871d99377130aa789775807c
nithin-chandran <nithinchandran.mec@gmail.com>
2024-08-29 23:40:03 +0530

    fixing username variable

--- terraform/packages/ecr/main.tf
+++ terraform/packages/ecr/main.tf
@@ -1,3 +1,7 @@
+locals {
+  user_name = element(split("/", var.principal_user), 1)
+}
+
 resource "aws_ecr_repository" "repository" {
   name                 = var.repository_name
   image_tag_mutability = "IMMUTABLE"
@@ -11,48 +15,44 @@ resource "aws_ecr_repository" "repository" {
 
 resource "aws_ecr_repository_policy" "policy" {
   repository = aws_ecr_repository.repository.name
-  policy     = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowPushPull",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "${var.principal_user}"
-      },
-      "Action": [
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:BatchGetImage",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:PutImage",
-        "ecr:InitiateLayerUpload",
-        "ecr:UploadLayerPart",
-        "ecr:CompleteLayerUpload"
-      ]
-    }
-  ]
-}
-EOF
+  policy = jsonencode({
+    "Version" : "2008-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowPushPull",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : var.principal_user
+        },
+        "Action" : [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+      }
+    ]
+  })
 }
 
 resource "aws_iam_user_policy" "ecr_auth" {
   name = "ecr_auth"
-  user = var.principal_user
+  user = local.user_name
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowGetAuthorizationToken",
-      "Effect": "Allow",
-      "Action": [
-        "ecr:GetAuthorizationToken"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowGetAuthorizationToken",
+        "Effect" : "Allow",
+        "Action" : [
+          "ecr:GetAuthorizationToken"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
 }
